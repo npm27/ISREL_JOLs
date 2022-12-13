bef972fa9119456093cf91c762c9dd61142a037c
Nicholas Maxwell <35810320+npm27@users.noreply.github.com>
2022-12-13 15:56:29 -0600

    Resolution

--- 1 Analyses/Ex 2/Analyze/Ex 2.R
+++ 1 Analyses/Ex 2/Analyze/Ex 2.R
@@ -6,6 +6,7 @@ dat = read.csv("Ex 2 Scored.csv")
 library(reshape)
 library(ez)
 library(psychReport)
+library(Hmisc)
 
 ##turn of scientific notation
 options(scipen = 999)
@@ -67,7 +68,6 @@ model1 = ezANOVA(long.dat,
                  wid = Sub.ID,
                  type = 3,
                  detailed = T)
-model1
 
 #main effect of "measure" (diff between JOLs and recall is NS)
 tapply(long.dat$Score, long.dat$measure, mean)
@@ -198,8 +198,314 @@ apply(post.RL.r, 2, sd)
 apply(post.READ.r, 2, mean) 
 apply(post.READ.r, 2, sd) 
 
-####Calibration Plots####
-
 ####Gamma####
+##get data in the right format
+long.dat.r = subset(long.dat,
+                    long.dat$measure == "Recall")
+long.dat.j = subset(long.dat,
+                    long.dat$measure == "JOL")
 
-####Write data to file for plot####
+combined = cbind(long.dat.r, long.dat.j)
+
+combined_gammas = combined[ , -c(5, 7:11)]
+
+colnames(combined_gammas)[5] = "Recall"
+colnames(combined_gammas)[6] = "JOL"
+colnames(combined_gammas)[2] = "type"
+colnames(combined_gammas)[1] = "Subject"
+
+combined_gammas = combined_gammas[ , c(1:4, 6, 5)]
+
+combined_gammas$Recall[combined_gammas$Recall == 100] = 1
+
+##Subset by encoding task
+gammas_IS = subset(combined_gammas,
+                   combined_gammas$type == "IS")
+
+gammas_RL = subset(combined_gammas,
+                   combined_gammas$type == "RL")
+
+gammas_READ = subset(combined_gammas,
+                     combined_gammas$type == "READ")
+
+##Okay, now subset everything by direction
+##IS
+gammas_IS_F = subset(gammas_IS,
+                     gammas_IS$Direction == "F")
+gammas_IS_B = subset(gammas_IS,
+                     gammas_IS$Direction == "B")
+gammas_IS_S = subset(gammas_IS,
+                     gammas_IS$Direction == "S")
+gammas_IS_U = subset(gammas_IS,
+                     gammas_IS$Direction == "U")
+
+##RL
+gammas_RL_F = subset(gammas_RL,
+                     gammas_RL$Direction == "F")
+gammas_RL_B = subset(gammas_RL,
+                     gammas_RL$Direction == "B")
+gammas_RL_S = subset(gammas_RL,
+                     gammas_RL$Direction == "S")
+gammas_RL_U = subset(gammas_RL,
+                     gammas_RL$Direction == "U")
+
+##READ
+gammas_READ_F = subset(gammas_READ,
+                       gammas_READ$Direction == "F")
+gammas_READ_B = subset(gammas_READ,
+                       gammas_READ$Direction == "B")
+gammas_READ_S = subset(gammas_READ,
+                       gammas_READ$Direction == "S")
+gammas_READ_U = subset(gammas_READ,
+                       gammas_READ$Direction == "U")
+
+###Now do a bunch of loops to get mean gammas (computes gamma for each participant then averages)
+##Start with IS
+#F
+empty = data.frame()
+
+for (i in unique(gammas_IS_F$Subject)){
+  
+  temp = subset(gammas_IS_F, gammas_IS_F$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+forward_IS = empty
+
+#Backward IS
+empty = data.frame()
+
+for (i in unique(gammas_IS_B$Subject)){
+  
+  temp = subset(gammas_IS_B, gammas_IS_B$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+backward_IS = empty
+
+#Symmetrical IS
+empty = data.frame()
+
+for (i in unique(gammas_IS_S$Subject)){
+  
+  temp = subset(gammas_IS_S, gammas_IS_B$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+symmetrical_IS = empty
+
+#Unrelated IS
+empty = data.frame()
+
+for (i in unique(gammas_IS_U$Subject)){
+  
+  temp = subset(gammas_IS_U, gammas_IS_U$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+Unrelated_IS = empty
+
+##Now do relational
+#F
+empty = data.frame()
+
+for (i in unique(gammas_RL_F$Subject)){
+  
+  temp = subset(gammas_RL_F, gammas_RL_F$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+forward_RL = empty
+
+#Backward
+empty = data.frame()
+
+for (i in unique(gammas_RL_B$Subject)){
+  
+  temp = subset(gammas_RL_B, gammas_RL_B$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+backward_RL = empty
+
+#Symmetrical
+empty = data.frame()
+
+for (i in unique(gammas_RL_S$Subject)){
+  
+  temp = subset(gammas_RL_S, gammas_RL_B$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+symmetrical_RL = empty
+
+#Unrelated
+empty = data.frame()
+
+for (i in unique(gammas_RL_U$Subject)){
+  
+  temp = subset(gammas_RL_U, gammas_RL_U$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+Unrelated_RL = empty
+
+##Now do read
+#F
+empty = data.frame()
+
+for (i in unique(gammas_READ_F$Subject)){
+  
+  temp = subset(gammas_READ_F, gammas_READ_F$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+forward_READ = empty
+
+#Backward
+empty = data.frame()
+
+for (i in unique(gammas_READ_B$Subject)){
+  
+  temp = subset(gammas_READ_B, gammas_READ_B$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+backward_READ = empty
+
+#Symmetrical
+empty = data.frame()
+
+for (i in unique(gammas_READ_S$Subject)){
+  
+  temp = subset(gammas_READ_S, gammas_READ_B$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+symmetrical_READ = empty
+
+#Unrelated
+empty = data.frame()
+
+for (i in unique(gammas_READ_U$Subject)){
+  
+  temp = subset(gammas_READ_U, gammas_READ_U$Subject == i)
+  
+  g = rcorr.cens(temp$Recall, temp$JOL, outx = TRUE)[2]
+  
+  g = unname(g)
+  
+  temp2 = data.frame(i, g)
+  
+  empty = rbind(temp2, empty)
+  
+}
+
+Unrelated_READ = empty
+
+##Okay, get means!
+#IS
+mean(forward_IS$g, na.rm = T)
+mean(backward_IS$g, na.rm = T)
+mean(symmetrical_IS$g, na.rm = T)
+mean(Unrelated_IS$g, na.rm = T)
+
+#RL
+mean(forward_RL$g, na.rm = T)
+mean(backward_RL$g, na.rm = T)
+mean(symmetrical_RL$g, na.rm = T)
+mean(Unrelated_RL$g, na.rm = T)
+
+#READ
+mean(forward_READ$g, na.rm = T)
+mean(backward_READ$g, na.rm = T)
+mean(symmetrical_READ$g, na.rm = T)
+mean(Unrelated_READ$g, na.rm = T)
